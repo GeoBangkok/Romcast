97847012004fa8af336385b422c033b62c81cbfc
GeoBangkok <42850725+GeoBangkok@users.noreply.github.com>
2026-01-29 10:56:44 -0500

    Initial Commit

---
+++ RomCast/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  RomCast
+//
+//  Created by standard on 1/29/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
